e1b94425334809c54efac5e26a9aad115d9fd2c8
Jamie Wagner <jamie@kajabi.com>
2020-10-31 00:21:17 -0700

    Add songs app

---
+++ songs/src/components/SongList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions';
+
+const SongList = (props) => {
+  const { selectSong, songs } = props;
+
+  const renderList = () => {
+    return songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="ui divided list">
+      {renderList()}
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  const { songs } = state;
+
+  return { songs };
+}
+
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
