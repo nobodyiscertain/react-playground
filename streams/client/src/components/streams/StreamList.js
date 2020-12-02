546ce63b4779d000759d9ae3978d402df8bac257
Jamie Wagner <hey@nobodyiscertain.com>
2020-12-01 22:30:19 -0800

    CRUD complete

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -50,7 +50,9 @@ const StreamList = (props) => {
           {renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`}>
+              {stream.title}
+            </Link>
             <div className="description">
               {stream.description}
             </div>
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
+const StreamShow = props => {
+  useEffect(() => {
+    props.fetchStream(props.match.params.id);
+  }, []);
+
+  if (!props.stream) {
+    return <div>Loading...</div>;
+  }
+
+  const { title, description } = props.stream;
+
   return (
-    <div>StreamShow</div>
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
   );
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
